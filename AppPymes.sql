a0ad92a2bd37b681e88f8dd3da3924f89ee201ab
Eleanarb <leangieriera@gmail.com>
2021-05-11 18:21:27 -0400

    Script base de datos

---
+++ AppPymes.sql
@@ -0,0 +1,283 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 11-05-2021 a las 22:20:57
+-- Versión del servidor: 5.5.24-log
+-- Versión de PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `AppPymes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_producto`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria_producto` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_pyme`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria_pyme` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE IF NOT EXISTS `ciudad` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  `id_region` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ciudad_fk_region` (`id_region`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `rut` varchar(20) NOT NULL,
+  `nombres` varchar(20) NOT NULL,
+  `apelidos` varchar(20) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `contrasena` varchar(20) NOT NULL,
+  `id_direccion` int(11) NOT NULL,
+  PRIMARY KEY (`rut`),
+  KEY `cliente_fk_direccion` (`id_direccion`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comuna`
+--
+
+CREATE TABLE IF NOT EXISTS `comuna` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  `id_ciudad` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comuna_fk_ciudad` (`id_ciudad`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direccion`
+--
+
+CREATE TABLE IF NOT EXISTS `direccion` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  `id_comuna` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `direccion_fk_comuna` (`id_comuna`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE IF NOT EXISTS `estado` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_pedido`
+--
+
+CREATE TABLE IF NOT EXISTS `estado_pedido` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE IF NOT EXISTS `pedidos` (
+  `boleta` int(11) NOT NULL,
+  `rut_cliente` varchar(20) NOT NULL,
+  `id_pyme` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha` varchar(20) NOT NULL,
+  `total` int(11) NOT NULL,
+  `id_estado_pedido` int(11) NOT NULL,
+  PRIMARY KEY (`boleta`),
+  KEY `pedidos_fk_pyme` (`id_pyme`),
+  KEY `pedidos_fk_cliente` (`rut_cliente`),
+  KEY `pedidos_fk_estado_pedido` (`id_estado_pedido`),
+  KEY `pedidos_fk_productos` (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(20) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `foto` varchar(20) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `id_pyme` int(11) NOT NULL,
+  `id_categoria_producto` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productos_fk_pyme` (`id_pyme`),
+  KEY `productos_fk_categoria_producto` (`id_categoria_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pyme`
+--
+
+CREATE TABLE IF NOT EXISTS `pyme` (
+  `id` int(11) NOT NULL,
+  `nombres` varchar(30) NOT NULL,
+  `apellidos` varchar(30) NOT NULL,
+  `rut` varchar(20) NOT NULL,
+  `nombre_pyme` varchar(20) NOT NULL,
+  `correo` varchar(40) NOT NULL,
+  `contrasena` varchar(20) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `id_categoria_pyme` int(11) NOT NULL,
+  `id_direccion` int(11) NOT NULL,
+  `id_estado` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pyme_fk_categoria_pyme` (`id_categoria_pyme`),
+  KEY `pyme_fk_direccion` (`id_direccion`),
+  KEY `pyme_fk_estado` (`id_estado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `region`
+--
+
+CREATE TABLE IF NOT EXISTS `region` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `validacion`
+--
+
+CREATE TABLE IF NOT EXISTS `validacion` (
+  `id` int(11) NOT NULL,
+  `id_pyme` int(11) NOT NULL,
+  `archivo` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `validacion_fk_pyme` (`id_pyme`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD CONSTRAINT `ciudad_fk_region` FOREIGN KEY (`id_region`) REFERENCES `region` (`id`);
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_fk_direccion` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id`);
+
+--
+-- Filtros para la tabla `comuna`
+--
+ALTER TABLE `comuna`
+  ADD CONSTRAINT `comuna_fk_ciudad` FOREIGN KEY (`id_ciudad`) REFERENCES `ciudad` (`id`);
+
+--
+-- Filtros para la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  ADD CONSTRAINT `direccion_fk_comuna` FOREIGN KEY (`id_comuna`) REFERENCES `comuna` (`id`);
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_fk_productos` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`),
+  ADD CONSTRAINT `pedidos_fk_cliente` FOREIGN KEY (`rut_cliente`) REFERENCES `cliente` (`rut`),
+  ADD CONSTRAINT `pedidos_fk_estado_pedido` FOREIGN KEY (`id_estado_pedido`) REFERENCES `estado_pedido` (`id`),
+  ADD CONSTRAINT `pedidos_fk_pyme` FOREIGN KEY (`id_pyme`) REFERENCES `pyme` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_fk_categoria_producto` FOREIGN KEY (`id_categoria_producto`) REFERENCES `categoria_producto` (`id`),
+  ADD CONSTRAINT `productos_fk_pyme` FOREIGN KEY (`id_pyme`) REFERENCES `pyme` (`id`);
+
+--
+-- Filtros para la tabla `pyme`
+--
+ALTER TABLE `pyme`
+  ADD CONSTRAINT `pyme_fk_estado` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id`),
+  ADD CONSTRAINT `pyme_fk_categoria_pyme` FOREIGN KEY (`id_categoria_pyme`) REFERENCES `categoria_pyme` (`id`),
+  ADD CONSTRAINT `pyme_fk_direccion` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id`);
+
+--
+-- Filtros para la tabla `validacion`
+--
+ALTER TABLE `validacion`
+  ADD CONSTRAINT `validacion_fk_pyme` FOREIGN KEY (`id_pyme`) REFERENCES `pyme` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
