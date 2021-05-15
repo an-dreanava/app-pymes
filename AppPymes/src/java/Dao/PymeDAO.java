905f23d996e93e759898a221577a76b9577c0471
an_dreanava <dreanavau@gmail.com>
2021-05-15 14:40:21 -0400

    Pyme

--- AppPymes/src/java/Dao/PymeDAO.java
+++ AppPymes/src/java/Dao/PymeDAO.java
@@ -5,10 +5,38 @@
  */
 package Dao;
 
+import Modelo.Conexion;
+import Modelo.Pyme;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author drean
  */
 public class PymeDAO {
+     PreparedStatement ps = null;
+    ResultSet rs = null;
     
+    private static ArrayList<String> regiones = new ArrayList<>();
+    
+       public ArrayList<String> Regiones(Pyme pyme) {
+           
+        Conexion con = new Conexion();
+        Connection conexion = con.getConnection();
+        try {
+            ps = conexion.prepareStatement("SELECT * FROM region");
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                regiones.add(rs.getString(1));
+            }
+            conexion.close();
+        } catch (Exception ex) {
+            System.err.println("Error, " + ex);
+        }
+        return regiones;
+    }
 }
