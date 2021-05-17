97785a2ddc79660cba3bd4c850f3e8bea51edf11
Eleanarb <leangieriera@gmail.com>
2021-05-16 20:40:34 -0400

    base de datos

--- localhost.sql
+++ localhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 12-05-2021 a las 14:05:49
+-- Tiempo de generación: 17-05-2021 a las 00:39:39
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
@@ -21,6 +21,23 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE `apppymes` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `apppymes`;
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_cliente`(IN `vRut` VARCHAR(20), IN `vNombres` VARCHAR(40), IN `vApellidos` VARCHAR(40), IN `vTelefono` VARCHAR(20), IN `vCorreo` VARCHAR(40), IN `vContraseña` VARCHAR(20), IN `vDireccion` VARCHAR(30), IN `vComuna` INT)
+    NO SQL
+begin
+
+select @id := max(id) + 1 from direccion;
+
+insert into direccion values (@id, vDireccion, vComuna);
+insert into cliente values (vRut, vNombres, vApellidos, vTelefono, vCorreo, vContraseña, @id);
+
+end$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -53,12 +70,28 @@ CREATE TABLE IF NOT EXISTS `categoria_pyme` (
 --
 
 CREATE TABLE IF NOT EXISTS `ciudad` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(20) NOT NULL,
   `id_region` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ciudad_fk_region` (`id_region`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Volcado de datos para la tabla `ciudad`
+--
+
+INSERT INTO `ciudad` (`id`, `descripcion`, `id_region`) VALUES
+(1, 'San José de Maipo', 7),
+(2, 'Santiago', 7),
+(3, 'Colina', 7),
+(4, 'Chicureo', 7),
+(5, 'Lampa', 7),
+(6, 'Buin', 7),
+(7, 'Paine', 7),
+(8, 'Talagante', 7),
+(9, 'Isla de Maipo', 7),
+(10, 'Peñaflor', 7);
 
 -- --------------------------------------------------------
 
@@ -74,8 +107,9 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `correo` varchar(30) NOT NULL,
   `contrasena` varchar(20) NOT NULL,
   `id_direccion` int(11) DEFAULT NULL,
+  `foto` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`rut`),
-  KEY `cliente_fk_direccion` (`id_direccion`)
+  KEY `id_direccion` (`id_direccion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -85,12 +119,50 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 --
 
 CREATE TABLE IF NOT EXISTS `comuna` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(20) NOT NULL,
   `id_ciudad` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `comuna_fk_ciudad` (`id_ciudad`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+
+--
+-- Volcado de datos para la tabla `comuna`
+--
+
+INSERT INTO `comuna` (`id`, `descripcion`, `id_ciudad`) VALUES
+(1, 'Santiago', 2),
+(2, 'Conchalí', 2),
+(3, 'Huechuraba', 2),
+(4, 'Independencia.', 2),
+(5, 'Quilicura', 2),
+(6, 'Recoleta', 2),
+(7, 'Renca', 2),
+(8, 'Las Condes', 2),
+(9, 'Lo Barnechea', 2),
+(10, 'Providencia', 2),
+(11, 'Vitacura', 2),
+(12, 'La Reina', 2),
+(13, 'Macul', 2),
+(14, 'Ñuñoa', 2),
+(15, 'Peñalolén', 2),
+(16, 'La Florida', 2),
+(17, 'La Granja', 2),
+(18, 'El Bosque', 2),
+(19, 'La Cisterna', 2),
+(20, 'La Pintana', 2),
+(21, 'San Ramón', 2),
+(22, 'Lo Espejo', 2),
+(23, 'Pedro Aguirre Cerda', 2),
+(24, 'San Joaquín', 2),
+(25, 'San Miguel', 2),
+(26, 'Cerrillos', 2),
+(27, 'Estación Central', 2),
+(28, 'Maipú', 2),
+(29, 'Cerro Navia', 2),
+(30, 'Lo Prado', 2),
+(31, 'Pudahuel', 2),
+(32, 'Quinta Normal', 2);
 
 -- --------------------------------------------------------
 
@@ -99,12 +171,12 @@ CREATE TABLE IF NOT EXISTS `comuna` (
 --
 
 CREATE TABLE IF NOT EXISTS `direccion` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(20) NOT NULL,
   `id_comuna` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `direccion_fk_comuna` (`id_comuna`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -203,10 +275,30 @@ CREATE TABLE IF NOT EXISTS `pyme` (
 --
 
 CREATE TABLE IF NOT EXISTS `region` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Volcado de datos para la tabla `region`
+--
+
+INSERT INTO `region` (`id`, `descripcion`) VALUES
+(1, 'Arica y Parinacota'),
+(2, 'Tarapacá'),
+(3, 'Antofagasta'),
+(4, 'Atacama'),
+(5, 'Coquimbo'),
+(6, 'Valparaíso'),
+(7, 'Metropolitana'),
+(8, 'Bernardo O''Higgins'),
+(9, 'Maule'),
+(10, 'Ñuble'),
+(11, 'Biobío'),
+(12, 'La Araucanía'),
+(13, 'Los Ríos'),
+(14, 'Los Lagos');
 
 -- --------------------------------------------------------
 
@@ -236,7 +328,7 @@ ALTER TABLE `ciudad`
 -- Filtros para la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  ADD CONSTRAINT `cliente_fk_direccion` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id`);
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `comuna`
@@ -254,9 +346,9 @@ ALTER TABLE `direccion`
 -- Filtros para la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  ADD CONSTRAINT `pedidos_fk_productos` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`),
   ADD CONSTRAINT `pedidos_fk_cliente` FOREIGN KEY (`rut_cliente`) REFERENCES `cliente` (`rut`),
   ADD CONSTRAINT `pedidos_fk_estado_pedido` FOREIGN KEY (`id_estado_pedido`) REFERENCES `estado_pedido` (`id`),
+  ADD CONSTRAINT `pedidos_fk_productos` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`),
   ADD CONSTRAINT `pedidos_fk_pyme` FOREIGN KEY (`id_pyme`) REFERENCES `pyme` (`id`);
 
 --
@@ -270,9 +362,9 @@ ALTER TABLE `productos`
 -- Filtros para la tabla `pyme`
 --
 ALTER TABLE `pyme`
-  ADD CONSTRAINT `pyme_fk_estado` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id`),
   ADD CONSTRAINT `pyme_fk_categoria_pyme` FOREIGN KEY (`id_categoria_pyme`) REFERENCES `categoria_pyme` (`id`),
-  ADD CONSTRAINT `pyme_fk_direccion` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id`);
+  ADD CONSTRAINT `pyme_fk_direccion` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id`),
+  ADD CONSTRAINT `pyme_fk_estado` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id`);
 
 --
 -- Filtros para la tabla `validacion`
